294cafb376dd4b975cbcee6d7bb5c5de0c250d29
chudomozg <chudomozg@gmail.com>
2020-03-11 17:17:15 +1000

    Initial commit

---
+++ title-from-id.php
@@ -0,0 +1,19 @@
+
+<?php
+/**
+ * Plugin Name: Title from ID
+ * Description: Плагин Wordpress, который позволяет автоматически генерировать Title (заголовок) страницы по введенному вами шаблону.
+ * Plugin URI:  Ссылка на инфо о плагине
+ * Author URI:  https://github.com/chudomozg
+ * Author:      Ivan Timoshenko <chudomozg@gmail.com>
+ * Version:     1.0
+ *
+ * Text Domain: Идентификатор перевода, указывается в load_plugin_textdomain()
+ * Domain Path: Путь до файла перевода. Нужен если файл перевода находится не в той же папке, в которой находится текущий файл.
+ *              Например, .mo файл находится в папке myplugin/languages, а файл плагина в myplugin/myplugin.php, тогда тут указываем "/languages"
+ *
+ * License:     GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * Network:     false
+ */
